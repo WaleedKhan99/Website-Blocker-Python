f65effad3e8a6d825635e6d7f203f77f46b455cf
Waleed Ahmed Khan <108469779+WaleedKhan99@users.noreply.github.com>
2024-08-26 11:38:53 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import datetime
+import time
+
+
+end_time = datetime.datetime("year","Month","Day")
+site_block = ["url of web"]
+host_path = ""
+redirect = ""
+
+
+while True:
+    if datetime.datetime.now()<end_time:
+        print("Start Blocking...")
+        with open(host_path, "r+") as host_file:
+            content  = host_file.read()
+            for website in site_block:
+                if website not in content:
+                    host_file.write(redirect + " " +website+"\n")
+                else:
+                    pass
+        
+    else:
+        with open(host_path, "r+") as host_file:
+            content = host_file.readlines()
+            host_file.seek(0)
+            for lines in content:
+                if not any(website in lines for website in site_block):
+                    host_file.write(lines)
+            
+            host_file.truncate()
+        time.sleep(5)
+        
+            
+                    
+            
+        
+        
+    
